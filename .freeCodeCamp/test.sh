b4ca1e313b63ce4f842892eea3db897b510e6507
Simiel Rocket <smenxah@gmail.com>
2025-05-25 22:29:23 +0000

    Save progress: 1412.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -8,9 +8,19 @@ N=$(( RANDOM % 6 ))
 echo ${RESPONSES[$N]}
 
 GET_FORTUNE(){
-  echo "Ask a yes or no question:"
+  if [[ ! $1 ]]
+  then
+    echo "Ask a yes or no question:"
+
+  else
+    echo "Try again. Make sure it ends with a question mark:"
+  fi
   read QUESTION
+  
 }
 
-GET_FORTUNE
-echo $QUESTION
+until [[ $QUESTION =~ \?$ ]]
+do
+  GET_FORTUNE
+done
+GET_FORTUNE again
